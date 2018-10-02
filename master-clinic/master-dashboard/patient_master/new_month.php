f6c80b7da73804060312ee0e6475832fc48f7db0
AlbanoBorba <albano.b06@gmail.com>
2018-10-02 14:17:47 -0300

    master clinic dashboard

---
+++ master-clinic/master-dashboard/patient_master/new_month.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>New Event</title>
+    	<!--link type="text/css" rel="stylesheet" href="media/layout.css" /-->    
+        <!-- helper libraries -->
+        <script src="../helpers/jquery-1.9.1.min.js" type="text/javascript"></script>
+    
+        <!-- daypilot libraries -->
+        <script src="../js/daypilot-all.min.js?v=2018.2.232" type="text/javascript"></script>
+    </head>
+    <body>
+        <?php
+            //require_once '_db.php';
+        
+            $start = $_GET['start'];
+            $end = $_GET['end'];
+        ?>
+        
+        <form id="f" style="padding:20px;">
+            <h1>Add consultie</h1>
+            <!--div><input type="text" id="name" name="name" value="" /></div-->
+            <div>Clinic: </div>
+            <select id="clinic" name="clinic">
+                <option value="1">Clinica 1</option>
+                <option value="2">Clinica 2</option>
+                <option value="3">Clinica 3</option>
+            </select>
+            <div>Medic: </div>
+            <select id="medic" name="medic">
+                <option value="1">Medic 1</option>
+                <option value="2">Medic 2</option>
+                <option value="3">Medic 3</option>
+            </select>
+            <div>Start:</div>
+            <div><input type="text" id="start" name="start" /></div>
+            <div>End:</div>
+            <div><input type="text" id="end" name="end" /></div>
+            
+            <div class="space"><input type="submit" value="Save" /> <a href="javascript:close();">Cancel</a></div>
+        </form>
+        
+        <script type="text/javascript">
+        function close(result) {
+            DayPilot.Modal.close(result);
+        }
+
+        $("#f").submit(function (ev) {
+            
+            DayPilot.Modal.close("foi");
+        });
+
+        $(document).ready(function () {
+            $("#name").focus();
+        });
+    
+        </script>
+    </body>
+</html>
---
+++ master-clinic/master-login/README.txt
@@ -0,0 +1,7 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/albanoborba/pen/dqxEYN.
+
+ Based on:
+- Flat Login Form (http://codepen.io/zavoloklom/pen/siKdh/)
+- Material Design by Google (http://www.google.com/design/)
+- Form (http://codepen.io/zavoloklom/full/yaozl)
+- Buttons (http://codepen.io/zavoloklom/pen/Gubja)
