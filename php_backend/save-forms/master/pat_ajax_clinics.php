0b8305cec42683730bad615e6cb7c729c88c0401
LzGustavo <xavier.luizgustavo@gmail.com>
2018-10-08 15:31:46 -0300

    patient new appt interface now filtering for specific clinics, doctor specs and available names

---
+++ php_backend/save-forms/master/pat_ajax_clinics.php
@@ -0,0 +1,19 @@
+<?php
+
+	require_once "../../class/storage.php";
+
+	session_start();
+
+	$db_instance = Storage::getInstance();
+
+	$db_instance->connect("GeracaoSaude");
+
+	$result = $db_instance->read_all("clinicas");
+
+	for ($i = 0; $i < count($result); $i++) {
+
+		echo "<option value='". $result[$i]["id"] . "'>". $result[$i]["id"]." - ".$result[$i]["nome"] ."</option>";
+	}
+
+	$db_instance->disconnect();
+?> 
